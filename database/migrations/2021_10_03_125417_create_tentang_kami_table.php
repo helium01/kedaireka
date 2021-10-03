6a805aa8949d9211d205a9932a65965951f7ecb3
fajar yuris wijayanto <fajarw935@gmail.com>
2021-10-03 22:35:00 +0700

    menambahkan bagiantemplate tampulan home admin controller dan migration database kemudian juga mengedit tampilan home

---
+++ database/migrations/2021_10_03_125417_create_tentang_kami_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTentangKamiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tentang_kami', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tentang_kami');
+    }
+}
